d8c69b3a2b803f8ced04a88432411b4106fa8f49
bad4iz <developer@bad4iz.ru>
2025-06-14 09:33:25 +0400

    test: add onClick invocation case for Parent Button

--- tests/Parent.spec.tsx
+++ tests/Parent.spec.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { shallow } from '../src';
 import { describe, expect, it } from 'vitest';
 import { vi } from 'vitest';
 import { shallow } from '../src';
@@ -16,7 +15,30 @@ describe('🐛 spec Parent', () => {
     const wrapper = shallow(<Parent onClick={() => {}} />);
 
     //❓ Assert
-    expect(wrapper.text()).toMatchSnapshot();
+    expect(wrapper.text()).toMatchInlineSnapshot(`
+      "<div>
+        <Children />
+        <Button />
+      </div>"
+    `);
+  });
+
+  it('🧪 textWithProps', () => {
+    expect.hasAssertions();
+    //☣️ Arrange (всякие моки)
+
+    // 🧹 clear mock
+
+    //🔥 Act
+    const wrapper = shallow(<Parent onClick={() => {}} />);
+
+    //❓ Assert
+    expect(wrapper.textWithProps()).toMatchInlineSnapshot(`
+      "<div>
+        <Children title="title" />
+        <Button onClick=function />
+      </div>"
+    `);
   });
 
   it('🧪 find', () => {
@@ -33,5 +55,20 @@ describe('🐛 spec Parent', () => {
     expect(wrapper.find('Button').props()).toStrictEqual({
       onClick,
     });
+  });
+
+  it('🧪 call fn', () => {
+    expect.hasAssertions();
+    //☣️ Arrange (всякие моки)
+    const onClick = vi.fn(() => 56);
+
+    // 🧹 clear mock
+
+    //🔥 Act
+    const wrapper = shallow(<Parent onClick={onClick} />);
+    const res = wrapper.find('Button').prop('onClick')();
+
+    //❓ Assert
+    expect(res).toBe(56);
   });
 });
